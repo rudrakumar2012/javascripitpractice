cc1d9a2d200b507d687a6d724c284345f1ebab06
Rudra Kumar <manaw10071994@gmail.com>
2024-03-11 11:42:09 +0530

    Fibonacci

---
+++ DSA/index.js
@@ -0,0 +1,13 @@
+/*function fibo(n) {
+  const fib = [0,1];
+  for(let i = 2; i<n ; i++){
+    fib[i] = fib[i-1] + fib[i-2];
+  }
+  return fib
+}
+
+console.log(fibo(2)); [0,1]
+console.log(fibo(3)); [0,1,1]
+console.log(fibo(7)); [0,1,1,2,3,5,8]
+Big-O: O(n) - Linear (because of 1 loop present in function)
+*/
