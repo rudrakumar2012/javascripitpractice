06e86a3e92476b79822ef0996e0e7d522fb0d2b4
rudrakumar2012 <manaw10071994@gmail.com>
2024-03-12 16:32:27 +0530

    Factorial

--- DSA/index.js
+++ DSA/index.js
@@ -1,4 +1,6 @@
-/*function fibo(n) {
+/*
+  *Fibonacci*
+  function fibo(n) {    
   const fib = [0,1];
   for(let i = 2; i<n ; i++){
     fib[i] = fib[i-1] + fib[i-2];
@@ -9,5 +11,17 @@
 console.log(fibo(2)); [0,1]
 console.log(fibo(3)); [0,1,1]
 console.log(fibo(7)); [0,1,1,2,3,5,8]
-Big-O: O(n) - Linear (because of 1 loop present in function)
+Big-O(Wrost Time Complexity): O(n) - Linear (because of 1 loop present in function)
+
+*Factorial*
+function factorial(n){
+  let result = 1;
+  for(let i = 2; i <= n; i++){
+    result = result * i;
+  }
+  return result;
+}
+
+console.log(factorial(5));
+Big-O: O(n) - Linear
 */
