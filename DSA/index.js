9abf91ed85577fe31a21dbe0061751582e0dad4d
rudrakumar2012 <manaw10071994@gmail.com>
2024-03-14 10:51:46 +0530

    Recursive Fibonacci

--- DSA/index.js
+++ DSA/index.js
@@ -79,3 +79,15 @@ console.log(isPowerOfTwo(8))
 
 Big-O: O(n) - Linear
 */
+
+function recursiveFibonacci(n) {
+  if(n < 2) {
+    return n
+  }
+  return recursiveFibonacci(n-1) + recursiveFibonacci(n-2)
+}
+
+console.log(recursiveFibonacci(0))
+console.log(recursiveFibonacci(1))
+console.log(recursiveFibonacci(6))
+console.log(recursiveFibonacci(7))
