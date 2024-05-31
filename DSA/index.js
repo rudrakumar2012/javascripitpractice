2c8e50e70110bd01bc270999c28e595219a92230
rudrakumar2012 <manaw10071994@gmail.com>
2024-05-31 10:55:50 +0530

    Linkedlist Class

--- DSA/index.js
+++ DSA/index.js
@@ -548,4 +548,31 @@ queue.print();
 queue.enqueue(60);
 queue.print();
 
+**Linked List**
+
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null
+    this.size = 0
+  }
+  isEmpty() {
+    return this.size === 0
+  }
+
+  getSize() {
+    return this.size
+  }
+}
+
+const list = new LinkedList()
+console.log('List is empty?', list.isEmpty())
+console.log('List size:', list.getSize())
+
 */
